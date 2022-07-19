c4f4bacfa60a57e8237ff23a474765fdb775721b
jonathancklee1 <jonathancklee@gmail.com>
2022-07-19 13:14:06 +1000

    Changed endpoints

--- src/spotify.js
+++ src/spotify.js
@@ -1,15 +1,10 @@
 const client_id = "51f88b9ea1154a2aa68e455cdf71ccbb";
-const redirect_uri = "http://localhost:3000";
+const redirect_uri = "https://jonathancklee1.github.io/music-player/";
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const scopes = [
   "user-library-read",
   "user-library-modify",
   "playlist-read-private",
-  "user-read-currently-playing",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-top-read",
-  "user-modify-playback-state",
 ];
 
 const loginEndpoint = `${authEndpoint}client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes.join(
